7b8c1c565f790821e6916a5533a632e573829bc1
Toothless <hyunjinney@naver.com>
2025-05-29 15:49:00 +0900

    [Bug] 코인, 스타 개수 동기화 문제
    - 스타 구매 시 해당 턴인 사람에게만 구매 창 팝업, 구매 후 결과 동기화
    - Scriptable OBject 로 코인, 스타 개수 관리

---
+++ Assets/ScriptableObject/DataSO.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[SerializeField]
+public class PlayerScores
+{
+    public int playerId;
+    public int Coins;
+    public int Stars;
+
+    public PlayerScores(int id, int a, int b)
+    {
+        playerId = id;
+        Coins = a;
+        Stars = b;
+    }
+}
+
+
+[CreateAssetMenu(fileName = "DataSO", menuName = "Scriptable Objects/DataSO")]
+public class DataSO : ScriptableObject
+{
+    public List<PlayerScores> playerScoreBoard = new();
+    public int clientCnt = 0;
+    public bool isFirst = false;
+    public bool isLBOpen = false;
+    [SerializeField] private Sprite[] _playerPortraits;
+    [SerializeField] private Sprite _defaultSprite;
+
+
+    public Action onScoreChangedAction;
+
+    private void OnEnable()
+    {
+        onScoreChangedAction += UpdateLeaderBoardClient;
+    }
+    private void OnDisable()
+    {
+        onScoreChangedAction -= UpdateLeaderBoardClient;
+    }
+    public void OnScoreChanged()
+    {
+        onScoreChangedAction?.Invoke();
+    }
+
+
+    string[] starResult = new string[4];
+    string[] coinResult = new string[4];
+    Sprite[] sprResult = new Sprite[4];
+
+    public void UpdateLeaderBoardClient() //score 바뀔 때마다 갱신해주는 함수 
+    {
+        playerScoreBoard = OrderingLeaderBoardClient();
+        for (int i = 0; i < clientCnt; i++)
+        {
+            PlayerScores ps = playerScoreBoard[i];
+            starResult[i] = ps.Stars.ToString();
+            coinResult[i] = ps.Coins.ToString();
+            sprResult[i] = _playerPortraits[ps.playerId];
+        }
+        for (int i = clientCnt; i < 4; i++)
+        {
+            sprResult[i] = _defaultSprite;
+        }
+        PlayerCurrentStateUIData pcData = new PlayerCurrentStateUIData
+        {
+            StarCountTxt = starResult,
+            CoinCountTxt = coinResult,
+            PlayerSprs = sprResult,
+        };
+
+        UIManager.Instance.CloseTargetUI<PlayerCurrentStateUI>();
+        UIManager.Instance.OpenPlayerCurrentUI(pcData);
+    }
+    public void OpenLeaderBoard() //리더보드 띄우는 함수
+    {
+        LeaderBoardUIData lbData = new LeaderBoardUIData
+        {
+            StarCountTxt = starResult,
+            CoinCountTxt = coinResult,
+            PlayerSprs = sprResult,
+            isAnimPlay = !isFirst
+            // FirstPlayerSpr ~ FourthPlayerSpr는 필요 시 설정
+        };
+        LeaderBoardManager.Instance.OpenLeaderBoardClient(lbData);
+    }
+    private List<PlayerScores> OrderingLeaderBoardClient()
+    {
+        return playerScoreBoard
+            .OrderByDescending(p => p.Stars)
+            .ThenByDescending(p => p.Coins)
+            .ToList();
+    }
+}
