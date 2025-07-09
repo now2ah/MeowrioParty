44585460a69131111fa7fda860a598705e6cfce4
now2ah <hhw3287@hanmail.net>
2025-07-09 17:09:23 +0900

    PlayerEntity 생성 / Coin 도메인 생성

---
+++ Assets/Scripts/Domains/Coin.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Meowrio.Domain
+{
+    /// <summary>
+    /// 플레이어 게임 진행에 필요한 코인 재화
+    /// </summary>
+    public class Coin
+    {
+        private int _value;
+        public int Value => _value;
+
+        public Coin(int value)
+        {
+            _value = value;
+        }
+
+        public void Add(int addValue)
+        {
+            _value += addValue;
+        }
+
+        public void Lose(int loseValue)
+        {
+            _value -= loseValue;
+
+            if (_value < 0)
+                _value = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Domains/PlayerEntity.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Meowrio.Domain
+{
+    public class PlayerEntity
+    {
+        private const int PLAYER_START_COIN_VALUE = 10;
+
+        private Coin _ownedCoins;
+
+        public PlayerEntity()
+        {
+            _ownedCoins = new Coin(PLAYER_START_COIN_VALUE);
+        }
+    }
+}
