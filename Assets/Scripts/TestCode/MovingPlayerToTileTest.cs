1282c9c32518568c1c25b7cc256547d4d857b530
now2ah <hhw3287@hanmail.net>
2025-07-09 17:46:53 +0900

    test code 임의 커밋

---
+++ Assets/Scripts/TestCode/MovingPlayerToTileTest.cs
@@ -0,0 +1,42 @@
+﻿using Meowrio.Domain;
+using Meowrio.Service;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.TestCode
+{
+    public class MovingPlayerToTileTest : MonoBehaviour
+    {
+        DiceService _diceService;
+        TileService _tileService;
+        PlayerEntity _player;
+
+        private void Awake()
+        {
+            _diceService = new DiceService(1, 6);
+
+            List<Tile> testTileList = new List<Tile>();
+            for (int i = 0; i < 8; ++i)
+            {
+                testTileList.Add(new GainCoinTile(3));
+            }
+            _tileService = new TileService(testTileList);
+
+            _player = new PlayerEntity();
+        }
+
+        private void Start()
+        {
+            Test();
+        }
+
+        public void Test()
+        {
+            int randomNum = _diceService.GetRandomDiceNumber();
+            Tile destinationTile = _tileService.TileList[randomNum];
+
+            _player.MoveTo(destinationTile);
+            destinationTile.ApplyEffect(_player);
+        }
+    }
+}
