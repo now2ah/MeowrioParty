23923d41b9eb3b01666701c78d985e464962f6dc
now2ah <hhw3287@hanmail.net>
2025-07-14 00:24:29 +0900

    round service 작업중

--- Assets/Scripts/Services/RoundService.cs
+++ Assets/Scripts/Services/RoundService.cs
@@ -3,5 +3,15 @@ namespace Meowrio.Service
 {
     public class RoundService
     {
+        private int _maxRound;
+        private int _roundCount;
+
+        public RoundService(int maxRound)
+        {
+            _maxRound = maxRound;
+            _roundCount = 0;
+        }
+
+        
     }
 }
